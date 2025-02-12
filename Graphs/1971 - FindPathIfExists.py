cfbbfa79c937e332ec17d12e0c11ed68467c2709
Bindhuu <143107119+Bindhhuu@users.noreply.github.com>
2025-02-12 07:37:49 +0530

    Create 1971 - FindPathIfExists.py

---
+++ Graphs/1971 - FindPathIfExists.py
@@ -0,0 +1,26 @@
+# Recursive DFS
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        if source == destination:
+            return True
+        
+        graph = defaultdict(list)
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        seen = set()
+        seen.add(source)
+
+        def dfs(i):
+            if i == destination:
+                return True
+            for n in graph[i]:
+                if n not in seen:
+                    seen.add(n)
+                    if dfs(n):
+                        return True
+            
+            return False
+        
+        return dfs(source)
