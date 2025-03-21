6f9525f755c3c8a2311df62d0022fa901d9821cf
Bindhuu <143107119+Bindhhuu@users.noreply.github.com>
2025-03-21 08:00:09 +0530

    Create 14 - LongestCommonPrefix.py

---
+++ Arrays/14 - LongestCommonPrefix.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        min_len = float('inf')
+
+        for s in strs:
+            if len(s) < min_len:
+                min_len = len(s)
+
+        i =0
+        while i < min_len:
+            for s in strs:
+                if s[i] != strs[0][i]:
+                    return s[:i]
+            i +=1
+        
+        return s[:i]
