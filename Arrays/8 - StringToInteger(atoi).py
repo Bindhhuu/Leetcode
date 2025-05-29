03fc03173e5c4a4e0479cec3e3c59008afee6a12
Bindhuu <143107119+Bindhhuu@users.noreply.github.com>
2025-05-29 13:22:27 +0530

    Update 8 - StringToInteger(atoi).py

--- Arrays/8 - StringToInteger(atoi).py
+++ Arrays/8 - StringToInteger(atoi).py
@@ -1,28 +1,27 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        maxx = 2**31
-        minn = 2**31-1
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+
         i = 0
+        n = len(s)
         result = 0
         sign = 1
-        n = len(s)
 
         while i < n and s[i] == ' ':
             i += 1
 
-            if sign == 1 and (s[i] == '-' or s[i] == '+'):
-                if s[i] == '-':
-                    sign = -1
-                else:
-                    sign = 1
-                i += 1
-        
+        if i < n and (s[i] == '+' or s[i] == '-'):
+            sign = -1 if s[i] == '-' else 1
+            i += 1
+
         while i < n and s[i].isdigit():
             digit = int(s[i])
-            if result > (maxx - digit) // 10:
-                return maxx if sign == 1 else minn
+            
+            if result > (INT_MAX - digit) // 10:
+                return INT_MAX if sign == 1 else INT_MIN
+            
             result = result * 10 + digit
             i += 1
-        return sign * result    
 
-
+        return sign * result
