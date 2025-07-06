8144dc15507075f8212f2f58015f8c89623986f7
Bindhuu <143107119+Bindhhuu@users.noreply.github.com>
2025-07-07 00:09:29 +0530

    Update 55 - JumpGame.py

--- Dynamic Programming/55 - JumpGame.py
+++ Dynamic Programming/55 - JumpGame.py
@@ -10,3 +10,18 @@ class Solution:
             if max_reach >= len(nums) - 1:  
                 return True
         return False
+
+#better approach
+from typing import List
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        target = n-1
+
+        for i in range(n-1, -1, -1):
+            maxx = nums[i]
+            if i+maxx >=target:
+                target = i
+        
+        return target == 0
