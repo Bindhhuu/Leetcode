a0ca17481475ac452271ddb2ec238be177ad2069
Bindhuu <143107119+Bindhhuu@users.noreply.github.com>
2025-07-09 06:57:34 +0530

    Update 48 - RotateImage.py

--- Arrays/48 - RotateImage.py
+++ Arrays/48 - RotateImage.py
@@ -1,19 +1,12 @@
-void rotate(int** matrix, int matrixSize, int* matrixColSize) {
-    int n= matrixSize;
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i+1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    for(int i=0; i<n; i++){
-        for(int j=i; j<n; j++){
-            int temp = matrix[i][j];
-            matrix[i][j] = matrix[j][i];
-            matrix[j][i] = temp;
-        }
-    }
-
-    for(int i=0; i < n; i++){
-        for(int j=0; j<n/2; j++){
-            int temp = matrix[i][j];
-            matrix[i][j] = matrix[i][n-j-1];
-            matrix[i][n-j-1] = temp;    
-        }
-    }
-}
+        for r in matrix:
+            r.reverse()
