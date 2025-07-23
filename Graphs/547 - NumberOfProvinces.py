8aeca095d9bee183aff2afc6a596e3af7f153405
Bindhuu <143107119+Bindhhuu@users.noreply.github.com>
2025-07-24 00:31:19 +0530

    Create 547 - NumberOfProvinces.py

---
+++ Graphs/547 - NumberOfProvinces.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        def dfs(node):
+            visited[node] = True
+            for neighbor in range(len(isConnected)):
+                if isConnected[node][neighbor] == 1 and not visited[neighbor]:
+                    dfs(neighbor)
+
+        n = len(isConnected)
+        visited = [False] * n
+        provinces = 0
+
+        for i in range(n):
+            if not visited[i]:
+                dfs(i)
+                provinces += 1
+
+        return provinces
