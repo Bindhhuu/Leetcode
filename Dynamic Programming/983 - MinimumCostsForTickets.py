ecf615f3454d090999d3e4dc6bb277b019585881
Bindhuu <143107119+Bindhhuu@users.noreply.github.com>
2025-09-07 16:58:09 +0530

    Create 983 - MinimumCostsForTickets.py

---
+++ Dynamic Programming/983 - MinimumCostsForTickets.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        n = days[-1]
+        dp = [0] * (n+1)
+        travel_days = set(days)
+
+        for i in range(1, n+1):
+            if i not in travel_days:
+                dp[i] = dp[i-1]
+            else:
+                dp[i] = min(
+                    dp[i-1] + costs[0],
+                    dp[max(0, i - 7)] + costs[1],
+                    dp[max(0, i - 30)] + costs[2]
+                )
+        return dp[n]
