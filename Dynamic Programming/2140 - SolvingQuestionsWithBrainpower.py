8b863e304f5ec59ce01adce7c04b77e948bc2d2a
Bindhuu <143107119+Bindhhuu@users.noreply.github.com>
2025-09-07 18:35:49 +0530

    Create 2140 - SolvingQuestionsWithBrainpower.py

---
+++ Dynamic Programming/2140 - SolvingQuestionsWithBrainpower.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        n = len(questions)
+        dp = [0] *(n+1)
+
+        for i in range(n-1, -1, -1):
+            points, brainpower = questions[i]
+            skip = dp[i+1]
+            solve = points + dp[min(n, i+1+brainpower)]
+            dp[i] = max(skip, solve)
+        return dp[0]
